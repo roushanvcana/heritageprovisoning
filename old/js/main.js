aa3c9bc7a45e42b6496f0bb48b304598f9a5aa67
rajroushan <roushan@vcanaglobal.com>
2021-12-30 12:47:51 +0530

    heritage live file

---
+++ old/js/main.js
@@ -0,0 +1,182 @@
+(function ($) {
+ "use strict";
+
+/*----------------------------
+ jQuery MeanMenu
+------------------------------ */
+	jQuery('nav#dropdown').meanmenu();	
+	
+/*----------------------------
+ wow js active
+------------------------------ */
+ new WOW().init();
+ 
+/*----------------------------
+ client owl active
+------------------------------ */  
+  $(".client-owl").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 6,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1399,6],
+	  itemsDesktopSmall : [980,4],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ trend owl active
+------------------------------ */  
+  $(".trend-item-slider").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 3,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,2],
+	  itemsDesktopSmall : [980,2],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+
+/*----------------------------
+ related owl active
+------------------------------ */  
+  $(".related-product-wrapper").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 6,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1399,5],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ section owl active
+------------------------------ */  
+  $(".tab-item-slider").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 5,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,4],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+    
+/*----------------------------
+ section owl two active
+------------------------------ */  
+  $(".tab-item-slider-two").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 5,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1399,3],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ testimonial owl active
+------------------------------ */  
+  $(".testimonial-owl-wrap").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 1,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1399,1],
+	  itemsDesktopSmall : [980,1],
+	  itemsTablet: [768,1],
+	  itemsMobile : [479,1],
+  });
+   
+/*--------------------------
+ Countdown
+---------------------------- */	
+    $('[data-countdown]').each(function() {
+        var $this = $(this), finalDate = $(this).data('countdown');
+        $this.countdown(finalDate, function(event) {
+        $this.html(event.strftime('<div class="cdown days"><span class="counting">%-D</span>days</div><div class="cdown hours"><span class="counting">%-H</span>hrs</div><div class="cdown minutes"><span class="counting">%M</span>mins</div><div class="cdown seconds"><span class="counting">%S</span>secs</div>'));
+        });
+    });
+    
+/*----------------------------
+ price-slider active
+------------------------------ */  
+    $( "#slider-range" ).slider({
+        range: true,
+        min: -80,
+        max: 460,
+        values: [ 25, 325 ],
+        slide: function( event, ui ) {
+		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+	   " - £" + $( "#slider-range" ).slider( "values", 1 ) ); 
+	  
+/*--------------------------
+ Elevatezoom 
+---------------------------- */	
+    
+    $("#zoom_01").elevateZoom();
+    $("#zoom_02").elevateZoom();
+    $("#zoom_03").elevateZoom();
+    $("#zoom_04").elevateZoom();
+    $("#zoom_05").elevateZoom();
+    $("#zoom_06").elevateZoom();
+      
+/*----------------------------------------------
+    Personal Address toggle function
+-----------------------------------------------*/
+    $("#add-new-address").on('click', function(){
+        $("#add-new-address-info").slideToggle();
+    });
+    
+    $("#transfer").on('click', function(){
+        $("#transfer-info").slideToggle();
+    });
+    
+    $("#payment").on('click', function(){
+        $("#payment-info").slideToggle();
+    });
+    
+    $("#paypal").on('click', function(){
+        $("#paypal-info").slideToggle();
+    });
+      
+/*--------------------------
+ scrollUp
+---------------------------- */	
+	$.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    }); 	   
+ 
+})(jQuery); 
---
+++ old/lib/custom-slider/home.js
@@ -0,0 +1,26 @@
+(function ($) {
+ "use strict";
+    
+		//---------------------------------------------
+		//Nivo slider
+		//---------------------------------------------
+			 $('#nivoslider').nivoSlider({
+				effect: 'random',
+				slices: 15,
+				boxCols: 8,
+				boxRows: 4,
+				animSpeed: 500,
+				pauseTime: 5000,
+				startSlide: 0,
+				directionNav: true,
+				controlNavThumbs: false,
+				 prevText: 'Pr <br /> ev',
+				 nextText: 'Ne <br /> xt', 
+				pauseOnHover: false,
+				manualAdvance: true,
+			 });
+			  
+           
+    
+			 
+})(jQuery); 
